62fdbda9eac9048b3d59fc5f4c205ae4a277a08d
yuan <yyz0832@163.com>
2016-03-28 15:50:21 +0800

    request 增加 获取IP 方法

--- src/Core/Request.php
+++ src/Core/Request.php
@@ -134,5 +134,17 @@ class Request
         } 
         return false;
     }
+    
+    public function getIp() {
+        $ip = '';
+        if (getenv('HTTP_CLIENT_IP')) {
+            $ip = getenv('HTTP_CLIENT_IP');
+        } else if (getenv('HTTP_X_FORWARDED_FOR')) {
+            $ip = getenv('HTTP_X_FORWARDED_FOR');
+        } else if (getenv('REMOTE_ADDR')) {
+            $ip = getenv('REMOTE_ADDR');
+        }
+        return $ip;
+    }
 
 }
