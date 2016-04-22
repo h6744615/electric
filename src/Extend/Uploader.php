60d9008a835466db0a470c124ef4e4d4bbb33e3a
罗云平 <luoyunping@windward.com.cn>
2016-04-22 14:49:12 +0800

    修改header

--- src/Extend/Uploader.php
+++ src/Extend/Uploader.php
@@ -241,6 +241,7 @@ class Uploader extends \Windward\Core\Base {
         }
         if ($flg) {
             if (($count = preg_match_all('#([w|h])(\d+)#i', $file, $m)) == 0) {
+                header('Content-Type: */*');
                 $fp = fopen($this->basePath . $file, 'rb');
                 fpassthru($fp);
                 exit;
