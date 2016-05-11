fe3eb5b256295dab80cdf0d396add990caa7b9b1
yuan <yyz0832@163.com>
2016-05-11 18:54:35 +0800

    smarty error tip

--- src/Mvc/View/Smarty/Plugins/function.error_tip.php
+++ src/Mvc/View/Smarty/Plugins/function.error_tip.php
@@ -6,16 +6,14 @@ function smarty_function_error_tip($params, &$smarty) {
     $class = isset($params['class']) ? $params['class'] : 'error';
     $errors = $params['errors'];
     $tmp = preg_split('/,/', $params['name'], -1, PREG_SPLIT_NO_EMPTY);
-    //$tag = isset($params['tag']) ? $params['tag'] : 'span';
-    $tag = '<div class="tags">&nbsp;&nbsp;&nbsp;<img class="label" src="/common/img/iconfont-tishi-2.png" alt=""/><div class="label-block"><div class="label-text">$message</div><div class="point"></div></div></div>';
+    $tag = isset($params['tag']) ? $params['tag'] : '<div class="tags">&nbsp;&nbsp;&nbsp;<img class="label" src="/common/img/iconfont-tishi-2.png" alt=""/><div class="label-block"><div class="label-text">$message</div><div class="point"></div></div></div>';
     if (empty($errors) || !$tmp) {
         return $result;
     }
 
     foreach ($tmp as $key) {
         if ($errors[$key]) {
-            //$result .= '<' . $tag . ' class="' . $class . '"' . '>' . $errors[$key] . '</' . $tag . '>';
-            $result .= str_replace('$message', $errors[$key], $tag);
+            $result .= isset($params['tag']) ? ('<' . $tag . ' class="' . $class . '"' . '>' . $errors[$key] . '</' . $tag . '>') : str_replace('$message', $errors[$key], $tag);
             break;
         }
     }
