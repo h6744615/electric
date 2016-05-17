ba839c917d46242287f4c8832bf19f0017e77e14
yuan <yyz0832@163.com>
2016-05-17 19:14:52 +0800

    lib\openssl

---
+++ src/Lib/Openssl.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Windward\Lib;
+
+class Openssl extends \Windward\Core\Base
+{
+
+    private $publicKey = '';
+    private $privateKey = '';
+
+    public function getPublicKey()
+    {
+        return $this->publicKey;
+    }
+
+    public function getPrivateKey()
+    {
+        return $this->privateKey;
+    }
+
+    public function setPublicKey($pk = '')
+    {
+        $this->publicKey = $pk;
+    }
+
+    public function setPrivateKey($pk = '')
+    {
+        $this->privateKey = $pk;
+    }
+
+    public function initKeyByFile($file = '')
+    {
+        if (!$file) {
+            return false;
+        }
+
+        $res = openssl_pkey_get_private("file://{$file}");
+        openssl_pkey_export($res, $privateKey);
+        $this->privateKey = $privateKey;
+        
+        $tmp = openssl_pkey_get_details($res);
+        $this->publicKey = $tmp['key'];
+    }
+
+    public function encrypt($data, $publicKey = '')
+    {
+        if (!$publicKey) {
+            $publicKey = $this->publicKey;
+        }
+        $encrypt = '';
+        openssl_public_encrypt($data, $encrypt, $publicKey);
+        return $encrypt;
+    }
+
+    public function decrypt($data, $privateKey = '')
+    {
+        if (!$privateKey) {
+            $privateKey = $this->privateKey;
+        }
+        $decrypt = '';
+        openssl_private_decrypt($data, $decrypt, $privateKey);
+        return $decrypt;
+    }
+
+}
