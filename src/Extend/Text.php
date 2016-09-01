f33a4bfef51e03454a2cd434b521674154a25058
yuan <yyz0832@163.com>
2016-09-01 14:59:35 +0800

    驼峰 转 下划线

--- src/Extend/Text.php
+++ src/Extend/Text.php
@@ -4,7 +4,7 @@ namespace Windward\Extend;
 
 class Text extends \Windward\Core\Base
 {
-    
+
     public static function camelCase($str)
     {
         if (strpos($str, '_') === false) {
@@ -12,4 +12,14 @@ class Text extends \Windward\Core\Base
         }
         return str_replace(' ', '', ucwords(str_replace('_', ' ', $str)));
     }
+
+    public static function underLine($str)
+    {
+        $camelCase = preg_replace_callback("/([A-Z]{1})/", function($match){
+            return "_" . strtolower($match[1]);
+        }, $str);
+        
+        return $camelCase;
+    }
+
 }
