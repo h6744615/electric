a2cc45b1fb32bf9ee90467f41386197d000e36a1
罗云平 <luoyunping@windward.com.cn>
2016-09-19 15:14:21 +0800

    单独处理测试加密

--- src/Lib/Aes.php
+++ src/Lib/Aes.php
@@ -6,24 +6,36 @@ class Aes
 {
 
     private $cipher;
-    
-    public function __construct()
+    private $padding;
+    private $dataWithIv;
+
+    public function __construct($padding = false, $dataWithIv = false)
     {
         $this->cipher = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
+        $this->padding = $padding;
+        $this->dataWithIv = $dataWithIv;
     }
 
-    public function encrypt($data, $key, $iv)
+    public function encrypt($data, $key, $iv = '')
     {
+        if ($this->dataWithIv && !$iv) {
+            $iv = openssl_random_pseudo_bytes(16);
+        }
         if (is_array($data)) {
             foreach ($data as $k => $v) {
                 $data[$k] = $this->encrypt($v, $key);
             }
         } elseif (strlen($data)) {
-            //$pad = 16 - (strlen($data) % 16);
-            //$data =  $data . str_repeat(chr($pad), $pad);
+            if ($this->padding) {
+                $pad = 16 - (strlen($data) % 16);
+                $data =  $data . str_repeat(chr($pad), $pad);
+            }
             if (mcrypt_generic_init($this->cipher, $key, $iv) != -1) {
                 $cipherText = mcrypt_generic($this->cipher, $data);
                 mcrypt_generic_deinit($this->cipher);
+                if ($this->dataWithIv) {
+                    $cipherText = $iv . $cipherText;
+                }
                 return base64_encode($cipherText);
             } else {
                 die('mcrypt init error');
@@ -33,7 +45,7 @@ class Aes
         return $data;
     }
 
-    public function decrypt($data, $key, $iv)
+    public function decrypt($data, $key, $iv = '')
     {
         if (is_array($data)) {
             foreach ($data as $k => $v) {
@@ -41,10 +53,17 @@ class Aes
             }
         } elseif (strlen($data)) {
             $data = base64_decode($data);
-
+            if ($this->dataWithIv) {
+                $iv = substr($data, 0, 16);
+                $data = substr($data, 16);
+            }
             if (mcrypt_generic_init($this->cipher, $key, $iv) != -1) {
                 $data = mdecrypt_generic($this->cipher, $data);
                 mcrypt_generic_deinit($this->cipher);
+                if ($this->padding) {
+                    $padLength = ord(substr($data, -1));
+                    $data = substr($data, 0, -$padLength);
+                }
                 return trim($data);
             } else {
                 die('mcrypt init error');
